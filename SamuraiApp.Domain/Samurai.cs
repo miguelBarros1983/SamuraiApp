c3ed1e849de6cee4e0f40d8a0ae190e709806f48
José Miguel Barros da Silva <miguelbarros1983@hotmail.com>
2018-06-21 16:18:08 +0100

    no message

---
+++ SamuraiApp.Domain/Samurai.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SamuraiApp.Domain
+{
+    public class Samurai
+    {
+        public Samurai()
+        {
+            Quotes = new List<Quote>();
+        }
+
+        public int Id { get; set; }
+
+        public  string Name { get; set; }
+
+        public List<Quote> Quotes { get; set; }
+
+        //public int BattleId { get; set; }
+
+        public List<SamuraiBattle> SamuraiBattles { get; set; }
+
+        public SecretIdentity SecretIidentity { get; set; }
+
+    }
+}
